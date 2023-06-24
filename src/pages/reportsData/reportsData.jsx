715850498d3b2fb92e91a542380fd24812449f4e
sanlokaja <sandyloka790@gmail.com>
2023-06-25 02:48:04 +0700

    docs: create pdf to get data

--- src/pages/reportsData/reportsData.jsx
+++ src/pages/reportsData/reportsData.jsx
@@ -49,7 +49,72 @@ const ReportsData = () => {
     /* -------------------- End Get Product -------------------- */
 
 
-    /* -------------------- Create PDF -------------------- */
+    /* -------------------- Get Category -------------------- */
+
+    const [category, setCategory] = useState([]);
+
+    const handleGetDownloadCategory = async () => {
+
+        const token = localStorage.getItem("token");
+
+        const categoriesDataRequest = await axios.get(
+            `http://localhost:2000/v1/category/search`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        const getCategory = await categoriesDataRequest.data.data.get_all_category;
+
+        setCategory(getCategory);
+    };
+
+
+    useEffect(() => {
+        
+        handleGetDownloadCategory();
+
+    }, []);
+
+    /* -------------------- Get Category -------------------- */
+
+
+    /* -------------------- Get Product Sale -------------------- */
+
+    const [productSaleData, setProductSaleData] = useState([]);
+
+    const onSearch = async () => {
+
+        const token = localStorage.getItem("token");
+
+        const productSaleDataRequest = await axios.get(
+            `http://localhost:2000/v1/sales/search`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Access-Control-Allow-Origin": "*"
+                },
+            }
+        );
+
+        const getProductSale = await productSaleDataRequest.data.data.get_all_product_sale;
+
+        setProductSaleData(getProductSale);
+    };
+
+    useEffect(() => {
+
+        onSearch();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    /* -------------------- End Get Product Sale -------------------- */
+
+
+    /* -------------------- Create PDF Product -------------------- */
 
     const createPDFToProductData = () => {
 
@@ -79,12 +144,7 @@ const ReportsData = () => {
 
             const tableHeaders = ['No', 'Nama', 'Kategori', 'Panjang', 'Stok', 'Price'];
             const tableData = productData.map((item, index) => [index + 1, item.name, item.category.categoryName, item.length, item.stock, item.price]);
-            // const tableSpacing = 10;
-            // const startX = doc.internal.pageSize.getWidth() / 2;
             const startY = 50;
-            // const cellWidth = 40;
-            // const cellHeight = 10;
-
 
             doc.autoTable({
                 head: [tableHeaders],
@@ -106,7 +166,117 @@ const ReportsData = () => {
         doc.save('product-reports.pdf', { autoDownload: false });
     };
 
-    /* -------------------- End Create PDF -------------------- */
+    /* -------------------- End Create PDF Product -------------------- */
+
+
+    /* -------------------- Create PDF Category -------------------- */
+
+    const createPDFToCategoryData = () => {
+
+        let doc;
+
+        if (!doc) {
+
+            doc = new jsPDF();
+
+            /* ------ Set Title ------ */
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(14);
+            doc.text('WarehouseHub', doc.internal.pageSize.getWidth() / 2, 20, { align: 'center' });
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(12);
+            doc.text('Laporan Data Kategori', doc.internal.pageSize.getWidth() / 2, 30, { align: 'center' });
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(12);
+            doc.text('Jalan Balai Rakyat 10, Jakarta Timur, DKI Jakarta', doc.internal.pageSize.getWidth() / 2, 40, { align: 'center' });
+
+            /* ------ End Set Title ------ */
+
+            /* ------ Set Table ------ */
+
+            const tableHeaders = ['No', 'Nama'];
+            const tableData = category.map((item, index) => [index + 1, item.categoryName]);
+            const startY = 50;
+
+            doc.autoTable({
+                head: [tableHeaders],
+                body: tableData,
+                startY: startY,
+                margin: { top: startY + 15 },
+                theme: 'grid',
+                headStyles: { fillColor: [0, 0, 0] },
+                alternateRowStyles: { fillColor: [235, 235, 235] },
+                columnStyles: { 0: { cellWidth: 15 } },
+            });
+
+            /* ------ End Set Table ------ */
+
+        }
+
+        // Unduh dokumen PDF
+
+        doc.save('category-reports.pdf', { autoDownload: false });
+    };
+    
+    /* -------------------- End Create PDF Category -------------------- */
+
+
+    /* -------------------- Create PDF Product Sale -------------------- */
+
+    const createPDFToProductSale = () => {
+
+        let doc;
+
+        if (!doc) {
+
+            doc = new jsPDF();
+
+            /* ------ Set Title ------ */
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(14);
+            doc.text('WarehouseHub', doc.internal.pageSize.getWidth() / 2, 20, { align: 'center' });
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(12);
+            doc.text('Laporan Data Produk Terjual', doc.internal.pageSize.getWidth() / 2, 30, { align: 'center' });
+
+            doc.setFont('Times New Roman');
+            doc.setFontSize(12);
+            doc.text('Jalan Balai Rakyat 10, Jakarta Timur, DKI Jakarta', doc.internal.pageSize.getWidth() / 2, 40, { align: 'center' });
+
+            /* ------ End Set Title ------ */
+
+            /* ------ Set Table ------ */
+
+            const tableHeaders = ['No', 'Nama', 'Kategori', 'Kode Transaksi', 'Tanggal', 'Total' ];
+            const tableData = productSaleData.map((item, index) => [index + 1, item.product.name, item.product.category.categoryName, item.transactionCode, item.salesDate, item.subTotal]);
+            const startY = 50;
+
+            doc.autoTable({
+                head: [tableHeaders],
+                body: tableData,
+                startY: startY,
+                margin: { top: startY + 15 },
+                theme: 'grid',
+                headStyles: { fillColor: [0, 0, 0] },
+                alternateRowStyles: { fillColor: [235, 235, 235] },
+                columnStyles: { 0: { cellWidth: 15 } },
+            });
+
+            /* ------ End Set Table ------ */
+
+        }
+
+        // Unduh dokumen PDF
+
+        doc.save('product-sale-reports.pdf', { autoDownload: false });
+    };
+    
+    /* -------------------- End Create PDF Category Product Sale -------------------- */
 
 
     return (
@@ -133,9 +303,9 @@ const ReportsData = () => {
                                     <Card.Title>Download Category Reports</Card.Title>
                                     <Card.Text>
                                         Jika anda memerlukan, anda dapat memperoleh salinan dari laporan
-                                        kategory dengan klik button "Download" di bawah ini.
+                                        kategori dengan klik button "Download" di bawah ini.
                                     </Card.Text>
-                                    <Button className="btn btn-download"> Download </Button>
+                                    <Button className="btn btn-download" onClick={createPDFToCategoryData}> Download </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -147,7 +317,7 @@ const ReportsData = () => {
                                         Jika anda memerlukan, anda dapat memperoleh salinan dari laporan
                                         penjualan dengan klik button "Download" di bawah ini.
                                     </Card.Text>
-                                    <Button className="btn btn-download"> Download </Button>
+                                    <Button className="btn btn-download" onClick={createPDFToProductSale}> Download </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
